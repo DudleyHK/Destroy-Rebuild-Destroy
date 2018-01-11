c258c35f64d44d0e6bb632c4529bd1e601466855
Dudley Dawes <dudley2.dawes@live.uwe.ac.uk>
2018-01-11 19:33:34 +0000

    Folder reorganise

---
+++ Assets/Game Objects/Aircraft/Scripts/Aircraft.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aircraft : MonoBehaviour 
+{
+    [SerializeField]
+    private Rigidbody aircraftRigidbody;
+
+
+
+
+
+
+    private void Awake()
+    {
+        aircraftRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+
+    private void Update()
+    {
+        Movement();
+    }
+
+
+    private void Movement()
+    {
+
+    }
+
+}
